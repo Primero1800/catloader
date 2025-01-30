9b8916df3116db82eca634ca40eec3ab7f044fc3
Primero1800 <Primero@inbox.ru>
2025-01-30 18:17:31 +0300

    completed docker-compose.yaml

--- catloader/main/tasks.py
+++ catloader/main/tasks.py
@@ -1,3 +1,5 @@
+import json
+
 import httpx
 import uuid
 
@@ -7,7 +9,44 @@ from django.conf import settings
 from catloader.celery import app
 
 #CAT_URL = "https://cataas.com/cat"
+
 CAT_URL = "https://thecatapi.com/api/images/get?format=src&type=gif"
+DOG_URL = "https://thedogapi.com/api/images/get?format=src&type=gif"
+
+
+def get_periodictask_name(task):
+    result = ''
+    headers = task.request.headers
+    if headers:
+        print(f"**********!!!!!!!!!!!!!!****************** {headers}, items: {headers.items()}")
+        result = headers.get('name', '')
+    else:
+        print("No headers available.")
+    return result
+
+
+def write_result(response, periodictask_name):
+    print(f"Inner result Response={response}")
+    try:
+        if response and response.status_code == 200:
+            file_ext = response.headers.get("Content-Type").split('/')[1]
+        else:
+            file_ext = '.err'
+        file_name = settings.BASE_DIR / 'cats' / (periodictask_name + str(uuid.uuid4()) + '.' + file_ext)
+
+        with open(file_name, 'wb') as file:
+            if response and response.status_code == 200:
+                for chunk in response.iter_bytes(chunk_size=256):
+                    file.write(chunk)
+            elif response:
+                file.write(f"{response.status_code} -- {response.headers} -- {response.content}".encode())
+            else:
+                file.write('No response error'.encode())
+    except Exception as exc:
+        print(response.status_code, periodictask_name, 'Error while writing file')
+        return False
+    return file_name
+
 
 @shared_task
 def add_ff(x, y):
@@ -16,34 +55,49 @@ def add_ff(x, y):
 
 @shared_task(bind=True)
 def import_image_of_cat(self, *args, **kwargs):
-    periodictask_name = ''
-    headers = self.request.headers
-    if headers:
-        print("Headers:")
-        print(f"********** {headers}, items: {headers.items()}")
-        [print(f"key={key}, value={value}") for key, value in headers.items()]
-        periodictask_name = headers.get('name', '')
-    else:
-        print("No headers available.")
+    periodictask_name = get_periodictask_name(self)
 
     with httpx.Client() as client:
         response = client.get(CAT_URL, follow_redirects=True)
 
-    print(f"Inner result Response={response}")
+    result_image = write_result(response=response, periodictask_name=periodictask_name)
+    if not result_image:
+        return None
+    return str(result_image)
 
-    if response and response.status_code == 200:
-        file_ext = response.headers.get("Content-Type").split('/')[1]
-    else:
-        file_ext = '.err'
-    file_name = settings.BASE_DIR / 'cats' / (periodictask_name + str(uuid.uuid4()) + '.' + file_ext)
 
-    with open(file_name, 'wb') as file:
-        if response and response.status_code == 200:
-            for chunk in response.iter_bytes(chunk_size=256):
-                file.write(chunk)
-        elif response:
-            file.write(f"{response.status_code} -- {response.headers} -- {response.content}".encode())
+@shared_task(bind=True)
+def import_image_of_dog(self, *args, **kwargs):
+    periodictask_name = get_periodictask_name(self)
+
+    with httpx.Client() as client:
+        response = client.get(DOG_URL, follow_redirects=True)
+
+    result_image = write_result(response=response, periodictask_name=periodictask_name)
+    if not result_image:
+        return None
+    return str(result_image)
+
+
+@shared_task(bind=True)
+def import_image_of_pet(self, *args, pet=None, **kwargs):
+    periodictask_name = get_periodictask_name(self)
+
+    if pet:
+        if pet == 'cat':
+            pet_url = CAT_URL
+        elif pet == 'dog':
+            pet_url = DOG_URL
         else:
-            file.write('No response error'.encode())
+            pet_url = None
+    if not pet or not pet_url:
+        print(periodictask_name, pet, pet_url)
+        return None
 
-    return str(file_name)
+    with httpx.Client() as client:
+        response = client.get(pet_url, follow_redirects=True)
+
+    result_image = write_result(response=response, periodictask_name=periodictask_name)
+    if not result_image:
+        return None
+    return str(result_image)
