58cf8970e9bce1cc46e5f20b8f6b4dbd36e17a04
Primero1800 <Primero@inbox.ru>
2025-01-30 18:29:12 +0300

    completed docker-compose.yaml

--- catloader/catloader/settings.py
+++ catloader/catloader/settings.py
@@ -159,6 +159,7 @@ CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'default'
 
 CELERY_RESULT_EXTENDED = True
+CELERY_RESULT_EXPIRE = 60 * 60
 
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
