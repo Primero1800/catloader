8dd9613b71c0b45d75c7b94b6252b9aa81e38751
Primero1800 <Primero@inbox.ru>
2025-02-07 13:10:37 +0300

    home changed

--- catloader/main/views.py
+++ catloader/main/views.py
@@ -6,9 +6,9 @@ from .tasks import add_ff, import_image_of_cat
 
 def home(request):
 
-    add_ff.delay(1, 2)
-    import_image_of_cat.delay()
+    # add_ff.delay(1, 2)
+    # import_image_of_cat.delay()
 
     return HttpResponse(
-        '<h1>Хайло</h1>'
+        '<center><h1>Hello. Visit admin page to operate</h1></center>'
     )
